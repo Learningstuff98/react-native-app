40defa90b3e8b7a67b2f53dc7151ab9ba90eceee
Andy Strube <andystrube64@gmail.com>
2020-08-13 17:35:34 -0700

    Added some data inputs

--- App.js
+++ App.js
@@ -1,22 +1,27 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default function App() {
   const [name, setName] = useState('jim');
-
-  const clickHandler = () => {
-    setName('bob');
-  }
+  const [age, setAge] = useState('30');
 
   return (
     <View style={styles.container}>
-      <Text>Your name is {name}</Text>
-      <View style={styles.buttonContainer}>
-        <Button 
-          title='update state'
-          onPress={clickHandler}
-        />
-      </View>
+      <Text>Enter name:</Text>
+      <TextInput
+        multiline
+        style={styles.input}
+        placeholder='joe'
+        onChangeText={(val) => setName(val)}
+      />
+      <Text>Enter age:</Text>
+      <TextInput
+        keyboardType='numeric'
+        style={styles.input}
+        placeholder='100'
+        onChangeText={(val) => setAge(val)}
+      />
+      <Text>name: {name}, age: {age}</Text>
     </View>
   );
 }
@@ -30,5 +35,12 @@ const styles = StyleSheet.create({
   },
   buttonContainer: {
     marginTop: 30
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#777',
+    padding: 8,
+    margin: 10,
+    width: 200
   }
 });
