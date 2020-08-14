daa94c7313ceddeec373b242b851fd64f57c4dde
Andy Strube <andystrube64@gmail.com>
2020-08-14 15:45:36 -0700

    Added a flat list

--- App.js
+++ App.js
@@ -1,35 +1,35 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 
 export default function App() {
   const [people, setPeople] = useState([
-    { name: 'bob', key: '1' },
-    { name: 'jim', key: '2' },
-    { name: 'bobjim', key: '3' },
-    { name: 'jimbob', key: '4' },
-    { name: 'bob', key: '5' },
-    { name: 'jim', key: '6' },
-    { name: 'bobjim', key: '7' },
-    { name: 'jimbob', key: '8' },
-    { name: 'bob', key: '9' },
-    { name: 'jim', key: '10' },
-    { name: 'bobjim', key: '11' },
-    { name: 'jimbob', key: '12' },
-    { name: 'bob', key: '13' },
-    { name: 'jim', key: '14' },
-    { name: 'bobjim', key: '15' },
-    { name: 'jimbob', key: '16' },
+    { name: 'bob', id: '1' },
+    { name: 'jim', id: '2' },
+    { name: 'bobjim', id: '3' },
+    { name: 'jimbob', id: '4' },
+    { name: 'bob', id: '5' },
+    { name: 'jim', id: '6' },
+    { name: 'bobjim', id: '7' },
+    { name: 'jimbob', id: '8' },
+    { name: 'bob', id: '9' },
+    { name: 'jim', id: '10' },
+    { name: 'bobjim', id: '11' },
+    { name: 'jimbob', id: '12' },
+    { name: 'bob', id: '13' },
+    { name: 'jim', id: '14' },
+    { name: 'bobjim', id: '15' },
+    { name: 'jimbob', id: '16' },
   ]);
 
-  return <ScrollView>
-    <View style={styles.container}>
-      {people.map(person => {
-        return <View key={person.key}>
-          <Text style={styles.person}>{person.name}</Text>
-        </View>
-      })}
-    </View>
-  </ScrollView>
+  return <View style={styles.container}>
+    <FlatList
+      numColumns={2}
+      data={people}
+      renderItem={({ item }) => (
+        <Text style={styles.person}>{item.name}</Text>
+      )}
+    />
+  </View>
 
 }
 
@@ -44,6 +44,8 @@ const styles = StyleSheet.create({
     marginTop: 24,
     padding: 30,
     backgroundColor: 'pink',
-    fontSize: 24
+    fontSize: 24,
+    marginHorizontal: 10,
+    marginTop: 24
   }
 });
