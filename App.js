1a02d627464e6bfd8bdc6105919f9c9f98822181
Andy Strube <andystrube64@gmail.com>
2020-08-17 18:23:11 -0700

    Added a header

--- App.js
+++ App.js
@@ -1,42 +1,27 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, FlatList, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+import Header from './components/header';
 
 export default function App() {
-  const [people, setPeople] = useState([
-    { name: 'bob', id: '1' },
-    { name: 'jim', id: '2' },
-    { name: 'bobjim', id: '3' },
-    { name: 'jimbob', id: '4' },
-    { name: 'bob', id: '5' },
-    { name: 'jim', id: '6' },
-    { name: 'bobjim', id: '7' },
-    { name: 'jimbob', id: '8' },
-    { name: 'bob', id: '9' },
-    { name: 'jim', id: '10' },
-    { name: 'bobjim', id: '11' },
-    { name: 'jimbob', id: '12' },
-    { name: 'bob', id: '13' },
-    { name: 'jim', id: '14' },
-    { name: 'bobjim', id: '15' },
-    { name: 'jimbob', id: '16' },
+  const [todos, setTodos] = useState([
+    { text: 'code', key: '1' },
+    { text: 'code some more', key: '2' },
+    { text: 'code even more', key: '3' }
   ]);
 
-  const pressHandler = (id) => {
-    setPeople((previousPeople) => {
-      return previousPeople.filter(person => person.id !== id);
-    });
-  }
-
   return <View style={styles.container}>
-    <FlatList
-      numColumns={2}
-      data={people}
-      renderItem={({ item }) => (
-        <TouchableOpacity onPress={() => pressHandler(item.id)}>
-          <Text style={styles.person}>{item.name}</Text>
-        </TouchableOpacity>
-      )}
-    />
+    <Header />
+    <View style={styles.content}>
+      {/* todo form */}
+      <View style={styles.list}>
+        <FlatList
+          data={todos}
+          renderItem={({ item }) => (
+            <Text>{item.text}</Text>
+          )}
+        />
+      </View>
+    </View>
   </View>
 
 }
@@ -44,16 +29,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    paddingTop: 40,
-    paddingHorizontal: 20
+    backgroundColor: '#fff'
   },
-  person: {
-    marginTop: 24,
-    padding: 30,
-    backgroundColor: 'pink',
-    fontSize: 24,
-    marginHorizontal: 10,
-    marginTop: 24
+  content: {
+    padding: 40
+  },
+  list: {
+    marginTop: 20
   }
 });
